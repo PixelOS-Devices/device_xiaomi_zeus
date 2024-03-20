1cca6deb5eebc7357c89c87f09b8c1b4629d2777
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-20 16:38:40 +0000

    dagda: Import Dolby Atmos blobs
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Icc9756e2d0646b60045f2456b3602683cd2a9dae

--- extract-files.sh
+++ extract-files.sh
@@ -31,6 +31,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
+       vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service)
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            ;;
         vendor/lib64/libkaraokepal.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "audio.primary.taro.so" "audio.primary.taro-zeus.so" "${2}"
